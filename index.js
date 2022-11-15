2a4d9b11e8ddf02d76b38a8d9fcf40dc1e39b3b3
Ongala morisho <mlgwebhost@gmail.com>
2022-11-15 07:16:13 +0200

    WIP: added json

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const shah512 = require('js-sha512');
+const port = 3000;
+const json = require('./public.json');
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
+app.get('/json', (req, res) => {
+    res.json(json);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
